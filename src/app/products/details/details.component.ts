76d968b8abf7638bbe66ba7410cfdb2eb7ec6537
Marius Axinte <axinte.marius@gmail.com>
2018-12-08 08:02:48 +0200

    Upgrade to angular 7

--- src/app/products/details/details.component.ts
+++ src/app/products/details/details.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../products/products.service';
 import { Product } from '../../products/product.model';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable } from "rxjs/Observable";
-import { switchMap, map } from "rxjs/operators";
+import { switchMap } from 'rxjs/operators';
 
-import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-details',
@@ -22,7 +20,7 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit() {
     this.route.paramMap
-      .switchMap((params: ParamMap) => this.productsService.getProduct(+params.get('id')))
+      .pipe(switchMap((params: ParamMap) => this.productsService.getProduct(+params.get('id'))))
       .subscribe(product => this.product = product);
   }
 
