611ccd4bb86af1b916d68a5163c4a3df8f80a80c
Wilhelm Grosz <wilhelm.grosz@iquestgroup.com>
2019-01-13 23:16:56 +0200

    updates angular to version 7.2

--- client/src/app/products/details/details.component.ts
+++ client/src/app/products/details/details.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../products/products.service';
 import { Product } from '../../products/product.model';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable } from "rxjs/Observable";
 import { switchMap, map } from "rxjs/operators";
-
-import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-details',
@@ -22,8 +19,10 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit() {
     this.route.paramMap
-      .switchMap((params: ParamMap) => this.productsService.getProduct(+params.get('id')))
-      .subscribe(product => this.product = product);
+      .pipe(
+        switchMap((params: ParamMap) => this.productsService.getProduct(+params.get('id')))
+      )
+      .subscribe((product: Product) => this.product = product);
   }
 
 }
--- client/src/app/products/products-edit.resolver.ts
+++ client/src/app/products/products-edit.resolver.ts
@@ -1,13 +1,11 @@
 import { Injectable } from "@angular/core";
 
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
-import { Observable } from "rxjs/Observable";
+import { Observable, of } from "rxjs";
 import { Product } from "./product.model";
 import { ProductsService } from "./products.service";
 
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/catch';
+import { take, map, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ProductsEditResolver implements Resolve<Product> {
@@ -18,19 +16,23 @@ export class ProductsEditResolver implements Resolve<Product> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<any> | Promise<any> | any {
-    
+
     let id = +route.paramMap.get('id');
 
     return this.productsService.getProduct(id)
-      .take(1)
-      .map(product => {
-        if(product) return product;
-        this.router.navigate(['dashboard', {message: "Prod not found"}]);
-        return null;
-      })
-      .catch(err => {
-        this.router.navigate(['dashboard', {message: "Prod not found"}]);
-        return Observable.of(null);
-      })
+      .pipe(
+        take(1),
+        map(product => {
+          if (product) {
+            return product;
+          }
+          this.router.navigate(['dashboard', {message: "Prod not found"}]);
+          return null;
+        }),
+        catchError(err => {
+          this.router.navigate(['dashboard', {message: "Prod not found"}]);
+          return of(null);
+        })
+      );
   }
 }
